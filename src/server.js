0160b63bd7cfe6d178c59f8799288210078687d3
a-nester <a_nester@ukr.net>
2024-07-30 10:42:59 +0300

    fix

--- src/server.js
+++ src/server.js
@@ -31,9 +31,10 @@ export const setupServer = () => {
     });
   });
 
-  app.get('/contacts/:contactId', async (req, res) => {
+  app.get('/contacts/:contactId', async (req, res, next) => {
     const { contactId } = req.params;
     const contact = await getContactById(contactId);
+
     if (!contact) {
       res.status(404).json({
         message: 'Contact not found',
