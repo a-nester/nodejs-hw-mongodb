81fb3fe2135724b403542491bc3902299e49aa8d
a-nester <a_nester@ukr.net>
2024-08-18 00:11:07 +0300

    fix/auth-register

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,10 +1,11 @@
 import createHttpError from 'http-errors';
-import { User } from '../db/models/user.js';
+
 import { registerUser } from '../services/auth.js';
+import { findUserByEmail } from '../services/auth.js';
 
 export const registerUserController = async (req, res, next) => {
   const { name, email } = req.body;
-  const user = User.findOne({ email });
+  const user = await findUserByEmail(email);
 
   if (user) throw createHttpError(409, 'Email in use!');
 
--- src/services/auth.js
+++ src/services/auth.js
@@ -1,8 +1,10 @@
 import { User } from '../db/models/user.js';
 import bcrypt from 'bcrypt';
 
+export const findUserByEmail = (email) => User.findOne({ email });
+
 export const registerUser = async (userData) => {
-  const encryptedPassword = bcrypt.hash(userData.password, 10);
+  const encryptedPassword = await bcrypt.hash(userData.password, 10);
 
   return await User.create({
     ...userData,
