b2a0021d8464e938391db71ccb1dcfb62889bbe3
a-nester <a_nester@ukr.net>
2024-09-20 18:47:54 +0300

    cors

--- src/server.js
+++ src/server.js
@@ -17,7 +17,26 @@ export const setupServer = () => {
   const app = express();
 
   app.use(express.json());
-  app.use(cors({ credentials: true }));
+  app.use(
+    cors({
+      origin: (origin, callback) => {
+        const allowedOrigins = [
+          'http://localhost:5173',
+          'http://localhost:5174',
+          'http://localhost:3000',
+          'http://localhost:4173',
+          'https://h2o-experts-app.vercel.app',
+          'https://h2o-experts-server.onrender.com',
+        ];
+        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+          callback(null, true);
+        } else {
+          callback(new Error('Not allowed by CORS'));
+        }
+      },
+      credentials: true,
+    }),
+  );
 
   app.use('/uploads', express.static(UPLOAD_DIR));
   app.use('/api-docs', swaggerDocs());
